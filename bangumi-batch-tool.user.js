dd6c8b4d87410398915712211ed3b4cbe3aa9cf1
凉 <121448906+liang0721gs@users.noreply.github.com>
2026-07-11 21:57:13 +0800

    Update bangumi-batch-tool.user.js

--- bangumi-batch-tool.user.js
+++ bangumi-batch-tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         批量修改条目收藏状态
 // @namespace    https://bgm.tv/
-// @version      1.0.1
+// @version      1.0.3
 // @description  在 Bangumi 目录页/收藏页提供批量修改收藏状态的功能
 // @author       liang0721gs
 // @include      /^https?:\/\/.*\.?(bgm\.tv|bangumi\.tv|chii\.in)\/index\/\d+/
